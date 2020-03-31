6395538b9016f73161df82d498c4ebb3e7986653
jmpfbmx <42691741+jmpfbmx@users.noreply.github.com>
2020-03-31 19:54:03 +0200

    Update Android.mk

--- Android.mk
+++ Android.mk
@@ -14,9 +14,9 @@
 # limitations under the License.
 #
 
-LOCAL_PATH := device/lenovo/aio_otfp
+LOCAL_PATH := device/HOMTOM/S8
 
-ifeq ($(TARGET_DEVICE),aio_otfp)
+ifeq ($(TARGET_DEVICE),S8)
 
 include $(call all-makefiles-under,$(LOCAL_PATH))
 
